b364c5220bb4a097d25f371515a77dd2bd2e2177
Oualid <jouhri.oualid@gmail.com>
2014-02-12 12:24:43 +0100

    add initializer in class font

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source "https://rubygems.org"
 
+# User Ruby 2.0
+ruby "2.0.0"
+
 # Declare your gem's dependencies in google_web_fonts.gemspec.
 # Bundler will treat runtime dependencies like base dependencies, and
 # development dependencies will be added by default to the :development group.
--- lib/google_web_fonts/font.rb
+++ lib/google_web_fonts/font.rb
@@ -4,19 +4,25 @@ require 'json'
 
 module GoogleWebFonts
 	class Font
-		def self.find_all()
-		uri = URI(" https://www.googleapis.com/webfonts/v1/webfonts?fields=items%2Ffamily&key=AIzaSyBd3teFVncpPyVBcs0sx1LLIC2PK0R2338")
-		http = Net::HTTP.new(uri.host,uri.port)
-		http.use_ssl = true
-		req = Net::HTTP::Get.new(uri.request_uri)
-		res = http.request(req)
-		res.body = JSON.parse(res.body) if res.body
-		fonts = res.body.values.first
-		available_fonts = Array.new
-		fonts.each do |font|
-			available_fonts << font.values.first
+
+		def initialize(key)
+			@@key = key
 		end
-		available_fonts
+
+
+		def self.find_all()
+			uri = URI("https://www.googleapis.com/webfonts/v1/webfonts?fields=items%2Ffamily&key=#{@@key}")
+			http = Net::HTTP.new(uri.host,uri.port)
+			http.use_ssl = true
+			req = Net::HTTP::Get.new(uri.request_uri)
+			res = http.request(req)
+			res.body = JSON.parse(res.body) if res.body
+			fonts = res.body.values.first
+			available_fonts = Array.new
+			fonts.each do |font|
+				available_fonts << font.values.first
+			end
+			available_fonts
 		end
 	end
 end
